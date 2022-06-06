29c1576d56e809c2273ae72a8b9687fdd5515c54
henok72 <taysonmohammed@gmail.com>
2022-06-06 07:10:14 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - prints this "with proper grammar, but the outcome is a piece of art,"
- * return: 0 if exited properly, non-zero otherwise.
+ * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
