dcad0058a9c2e8477dc2b35ebc11de5155c155fa
henok72 <taysonmohammed@gmail.com>
2022-06-09 07:03:39 -0700

    adding files

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,6 +1,8 @@
-#include <stdio.h>
+#include "main.h"
+
 /**
  * _abs -computes the absolute value of an integer
+ *
  * Return: the absolute value of int
  */
 int _abs(int n)
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,9 +1,8 @@
-#include "holberton.h"
-
-
+#include "main.h"
 
 /**
  * print_last_digit - prints the last digit of a number
+ *
  * Return: value of the last digit
  */
 int print_last_digit(int n)
