2741ef221c34caffb3f23cbfffdf33dd391c9bbe
henok72 <taysonmohammed@gmail.com>
2022-06-09 03:33:56 -0700

    adding main file

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,15 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * print_alphabet - prints the alphabet, in lowercase, followed by a new line
  */
 void print_alphabet(void)
 {
-	char ch;
+	int i;
 
-	ch = 'a';
-	while (ch <= 'z')
+	for (i = 97; i <= 122; i++)
 	{
-	_putchar(ch);
-	ch++;
+		_putchar (i);
 	}
-	_putchar('\n');
+	_putchar("\n");
 }
